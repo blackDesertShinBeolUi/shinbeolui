a3389fb7909fc74b40cc7384820fc064ca49068d
blackDesertShinBeolUi <yanghye0321@naver.com>
2024-02-29 16:09:53 +0900

    2024-02-29 최초 커밋

---
+++ shinbeolui/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          ShinBeolUi
+        </p>
+        <p>
+          BLACKDSERTONLINE
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
